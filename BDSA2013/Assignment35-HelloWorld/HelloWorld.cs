03931a1eea83d13c759e07a8422fb12501518a27
Rabka <smot@itu.dk>
2014-09-09 13:31:37 +0200

    Start

---
+++ BDSA2013/Assignment35-HelloWorld/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment35_HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            // Output text to the screen.
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BDSA2013/Assignment35-IsPowerOf/AIsPowerOfB.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment35_IsPowerOf
+{
+    class AIsPowerOfB
+    {
+
+        static bool IsPowerOf(int a, int b)
+        {
+            if (a == 1 || b == 1)
+                return true;
+
+            if (a % b > 0)
+                return false;
+
+            a = a / b;
+
+            return IsPowerOf(a, b);
+        }
+
+        static void Main(string[] args)
+        {
+
+            if (args.Length == 2)
+            {
+                Console.WriteLine("Is {0} a power of {1}:", args[0], args[1]);
+
+                int a = 0, b = 0;
+                try
+                {
+                    a = Convert.ToInt32(args[0]);
+                    b = Convert.ToInt32(args[1]);
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Input string is not a sequence of digits.");
+                }
+
+                Console.Out.WriteLine(IsPowerOf(a, b));
+            }
+            else
+            {
+                Console.WriteLine("Not enough arguments / or to many");
+            }
+            Console.ReadKey();
+        }
+    } 
+}
