5e390b6de78c41eec7c2afd03999096353a72dce
Rabka <smot@itu.dk>
2014-10-16 19:52:02 +0200

    her entity classer

---
+++ BDSA2014_VSC#/Assignment40_PartIII/Assignment40_PartIII/Model/NorthWindDataModel.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\stin7054\Documents\GitHub\BDSA2014\BDSA2014_VSC#\Assignment40_PartIII\Assignment40_PartIII\Model\NorthWindDataModel.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
