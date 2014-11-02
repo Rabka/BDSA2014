9e47cdc25c6eb3acf4ce6a87248f975c3aa949ff
patr0805 <puritymail@gmail.com>
2014-11-02 12:17:17 +0100

    created unit test project

---
+++ Assinments/Pebj_Smot_AS.43/NorthWindVisualizer (Part II)/NorthWindVisualizer.Tests/ModelViewTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NorthWindVisualizer.Tests
+{
+    [TestClass]
+    public class ModelViewTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
