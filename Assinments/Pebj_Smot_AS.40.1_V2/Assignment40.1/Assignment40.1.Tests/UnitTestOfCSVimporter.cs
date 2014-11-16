4cd6e6d85eb5e4c48dff5d7b4f48787583c9efae
Rabka <smot@itu.dk>
2014-11-16 13:24:12 +0100

    her

--- Assinments/Pebj_Smot_AS.40.1_V2/Assignment40.1/Assignment40.1.Tests/UnitTestOfCSVimporter.cs
+++ Assinments/Pebj_Smot_AS.40.1_V2/Assignment40.1/Assignment40.1.Tests/UnitTestOfCSVimporter.cs
@@ -30,16 +30,28 @@ namespace Assignment40_1.Tests
         /// Tests if ther is the rigth number of Categorys.
         /// </summary>
         [TestMethod]
-        public void TestForNumberOfCategorys()
+        public void TestDataCorrectImported()
         {
-            Assert.AreEqual(8, respiratory.Categories().Count());
+            // for the one categori
+            Assert.AreEqual(1, respiratory.Categories().First().CategoryID);
+            Assert.AreEqual("TestBeverages", respiratory.Categories().First().CategoryName);
+            Assert.AreEqual("Test Soft drinks, coffees, teas, beers, and ales", respiratory.Categories().First().Description);
+
+            // for the two order_details
+            Assert.AreEqual(1, respiratory.Products().First().Order_Details.Select(x => x).Where((x =>x.OrderID == 1)).First().ProductID);
+            Assert.AreEqual(100, respiratory.Products().First().Order_Details.Select(x => x).Where((x => x.OrderID == 1)).First().UnitPrice);
+            Assert.AreEqual(12, respiratory.Products().First().Order_Details.Select(x => x).Where((x => x.OrderID == 1)).First().Quantity);
+            Assert.AreEqual(10, respiratory.Products().First().Order_Details.Select(x => x).Where((x => x.OrderID == 1)).First().Discount);
         }
 
         /// <summary>
         /// Tests if ther is the rigth number of Orders.
+        /// OrderID;ProductID;UnitPrice;Quantity;Discount
+        /// 1;1;100;12;10
+        /// 2;1;100;10;0
         /// </summary>
         [TestMethod]
-        public void TestForNumberOfOrders()
+        public void TestCorrectNumberOfEntries()
         {
             Assert.AreEqual(830, respiratory.Orders().Count());
         }
